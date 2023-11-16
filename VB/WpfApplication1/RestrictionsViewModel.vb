f2caf61e6adf8eae9602c0d357b062ab481d137f
DevExpressExampleBot <fakeEmail>
2023-11-16 16:31:15 +0400

    autogenerate VB

--- VB/WpfApplication1/RestrictionsViewModel.vb
+++ VB/WpfApplication1/RestrictionsViewModel.vb
@@ -1,39 +1,46 @@
-﻿Imports System
 Imports System.Collections.ObjectModel
-Imports System.Linq
 Imports DevExpress.Mvvm.POCO
-Imports DevExpress.XtraScheduler
 
 Namespace WpfApplication1
-	Public Class RestrictionsViewModel
-		Private ReadOnly data As New SampleDataGenerator()
-		Protected Sub New()
-			AppointmentsPerDay = 10
-			DayCount = 2
-			ResourceCount = 3
-		End Sub
 
-		Public Overridable ReadOnly Property Appointments() As ObservableCollection(Of AppointmentData)
-			Get
-				Return data.Appointments
-			End Get
-		End Property
-		Public Overridable ReadOnly Property Resources() As ObservableCollection(Of ResourceData)
-			Get
-				Return data.Resources
-			End Get
-		End Property
-		Public Overridable Property AppointmentsPerDay() As Integer
-		Public Overridable Property DayCount() As Integer
-		Public Overridable Property ResourceCount() As Integer
-		Protected Sub OnDayCountChanged()
-			data.SetUp(DayCount, ResourceCount, AppointmentsPerDay)
-		End Sub
-		Protected Sub OnResourceCountChanged()
-			data.SetUp(DayCount, ResourceCount, AppointmentsPerDay)
-		End Sub
-		Public Shared Function Create() As RestrictionsViewModel
-			Return ViewModelSource.Create(Function() New RestrictionsViewModel())
-		End Function
-	End Class
+    Public Class RestrictionsViewModel
+
+        Private ReadOnly data As SampleDataGenerator = New SampleDataGenerator()
+
+        Protected Sub New()
+            AppointmentsPerDay = 10
+            DayCount = 2
+            ResourceCount = 3
+        End Sub
+
+        Public Overridable ReadOnly Property Appointments As ObservableCollection(Of AppointmentData)
+            Get
+                Return data.Appointments
+            End Get
+        End Property
+
+        Public Overridable ReadOnly Property Resources As ObservableCollection(Of ResourceData)
+            Get
+                Return data.Resources
+            End Get
+        End Property
+
+        Public Overridable Property AppointmentsPerDay As Integer
+
+        Public Overridable Property DayCount As Integer
+
+        Public Overridable Property ResourceCount As Integer
+
+        Protected Sub OnDayCountChanged()
+            data.SetUp(DayCount, ResourceCount, AppointmentsPerDay)
+        End Sub
+
+        Protected Sub OnResourceCountChanged()
+            data.SetUp(DayCount, ResourceCount, AppointmentsPerDay)
+        End Sub
+
+        Public Shared Function Create() As RestrictionsViewModel
+            Return ViewModelSource.Create(Function() New RestrictionsViewModel())
+        End Function
+    End Class
 End Namespace
